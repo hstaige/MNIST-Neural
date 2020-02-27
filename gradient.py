01408cf08045ec8d591b1f93ce5dbbc6496d654f
staigerh1 <staigerh1@gmail.com>
2020-02-27 14:58:27 -0500

    Modularized functions

--- gradient.py
+++ gradient.py
@@ -1,9 +1,14 @@
-import numpy as np
-
-layer3Len = 10
-layer2Len = 28
+from neededModules import *
 
 def createJacobian(A3):
+    """
+    Creates a jacobian matrix used to calculate B2 gradient. Represents how
+    changing one input value to the last layer changes the output activations.
+
+    INPUT: A3, [layer3len,1]
+
+    OUTPUT: Jacobian matrix, [layer3len,layer3len]
+    """
     jacob = np.zeros((layer3Len,layer3Len))
     for i in range(layer3Len):
         for j in range(layer3Len):
@@ -15,16 +20,26 @@ def createJacobian(A3):
 
 
 def sigmoidGradient(A):
+    """
+    Calculates the gradient of the activations of the sigmoid activated layers
+    with respect to their input.
+
+    INPUT: A, [layerlen, 1]
+
+    OUTPUT: sigmoid gradient, [layerlen, 1]
+    """
     gradient = np.multiply(A,(-A+1))
-    # if np.amax(gradient) == 0:
-    #     A = A - 0.001
-    #     gradient = np.multiply(A,(-A+1))
-    #     return gradient
-    # else:
     return gradient
 
 
 def calcB2grad(y_train,A3):
+    """
+    Calculates the gradient of the cost with respect to B2.
+
+    INPUT: y_train, [layer3len,1] ; A3, [layer3len,1]
+
+    OUTPUT: B2grad, [layer3len,1]
+    """
     costGrad = A3-y_train #numCategories by 1 vector
     B2Jacobian = createJacobian(A3) #A3 by A3 matrix
     B2grad = np.dot(costGrad.transpose(),B2Jacobian) #how changing something in b2
@@ -33,13 +48,29 @@ def calcB2grad(y_train,A3):
 
 
 def calcW2grad(B2Grad, A2):
-    temp1 = B2Grad.reshape((10,1))
-    temp2 = A2.reshape((1,28))
+    """
+    Calculates the gradient of the cost with respect to W2
+
+    INPUT: B2Grad, [layer3len, 1] ; A2, [layer2len, 1]
+
+    OUTPUT: W2grad [layer3len, layer2Len]
+    """
+    temp1 = B2Grad.reshape((layer3Len,1))
+    temp2 = A2.reshape((1,layer2Len))
     W2grad = np.dot(temp1, temp2 )
     return W2grad
 
 
 def calcB1grad(B2grad,W2,A2):
+    """
+    Calculates the gradient of the cost with respect to B1 using the chain rule
+
+    INPUT: B2grad, [layer3Len,1] ; W2, [layer2Len, layer3Len] ;
+           A2, [layer2len, 1]
+
+    OUTPUT: B1grad, [layer2Len, 1]
+
+    """
     temp1 = np.dot(W2,B2grad) #layer2Len * 1 vector
     sigmGradient = sigmoidGradient(A2) #layer2len * 1 vector
     B1grad = np.multiply(sigmGradient,temp1)
@@ -47,8 +78,17 @@ def calcB1grad(B2grad,W2,A2):
 
 
 def calcW1Grad(B1grad, x_train):
-    temp1 = B1grad.reshape((28,1))
-    temp2 = x_train.reshape((1,784))
+    """
+    Calculates the gradient of the cost with respect to W1 using the chain rule
+
+    INPUT: B1grad, [layer2Len,1] ; x_train, [layer1Len, 1] ;
+
+    OUTPUT: W1grad, [layer1Len, layer2Len]
+
+    """
+
+    temp1 = B1grad.reshape((layer2Len,1))
+    temp2 = x_train.reshape((1,layer1Len))
     W1grad = np.dot(temp1,temp2)
     return W1grad
 
@@ -60,3 +100,4 @@ def gradientCalculations(x_train,y_train,A2,A3,W1,B1,W2,B2):
     W1grad = calcW1Grad(B1grad, x_train)
 
     return [W1grad.transpose(), B1grad.transpose(), W2grad.transpose(), B2grad.transpose()]
+    #note, I dont know why i have to transpose them but it works `\(0_0)/`
--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import tensorflow as tf
-import numpy as np
-import random
-from gradient import *
-import keyboard
-import matplotlib.pyplot as plt
+from neededModules import * #contains imports and constants
+from gradient import * #calculates gradient of network
+from Intializer import * #loads data and randomizes starting weights
 from WorkbooksTests import saveData
 
 #fills a np array of the specificied size with random values 0<x<1
-def fillWithRands(dim1,dim2=1):
-    temp = np.zeros((dim1,dim2))
-    for i in range(dim1):
-        for j in range(dim2):
-            temp[i,j] = random.randint(-100,100)/100
-    return temp
-
 def calcZVals(Weights,Actives,Biases):
     temp = np.dot(Actives,Weights) #multiply
     temp = temp + Biases
@@ -34,52 +24,15 @@ def getCost(A,labels): #squared error
     squaredSum = np.sum(squared)/2
     return squaredSum
 
-gradMomentum = 0 #initial value
-keepingOn = False
-numLayers = 3
-numCategories = 10
 
-numTrained = 40
+(x_train,y_train),(x_test,y_test) = loadStartingData()
 
-numTested = 10000
-
-layer1Len = 784
-layer2Len = 28
-layer3Len = 10
-
-momentum = 0.8
-
-learn = -0.13
-learn = learn/numTrained
-
-(x_train,y_train),(x_test,y_test) = tf.keras.datasets.mnist.load_data()
-
-x_train = x_train.reshape((len(x_train), 784))
-x_test = x_test.reshape((len(x_test),784))
-
-#converts y_train from labels to list of probabilities
-y_temp = np.zeros((len(y_train),numCategories))
-for idx in range(len(y_train)):
-    y_temp[idx,y_train[idx]] = 1
-y_train = y_temp
-
-#converts y_test from labels to list of probabilities
-y_temp = np.zeros((len(y_test),numCategories))
-for idx in range(len(y_test)):
-    y_temp[idx,y_test[idx]] = 1
-y_test = y_temp
-
-#creates random weights and biases
-W1 = fillWithRands(layer1Len,layer2Len) #L1 by L2 matrix
-B1 = fillWithRands(layer2Len).flatten() #L2 by 1 vector
-
-W2 = fillWithRands(layer2Len,layer3Len) #L2 by L3 matrix
-B2 = fillWithRands(layer3Len).flatten() # L3 by 1 vector
+W1,B1,W2,B2 = createWeightsAndBiases()
 
 chosenEpochs = []
 chosenCosts = []
 #essentially a while true with a built in exit
-for epoch in range(20000):
+for epoch in range(80000):
     W1temp = np.zeros((layer1Len,layer2Len)) #L1 by L2 matrix
     B1temp =  np.zeros(layer2Len).flatten() #L2 by 1 vector
 
@@ -146,4 +99,6 @@ for idx in range(numTested):
 print(right/tested)
 
 plt.plot(chosenEpochs,chosenCosts)
+titleString = 'Cost vs Epochs for momentum: ' + str(momentum)
+plt.title(titleString)
 plt.savefig('Epoch_vs_Cost.png')
